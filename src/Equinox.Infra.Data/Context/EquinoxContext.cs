97b5e0ca3a36e701e19523114600d881ea6ba713
JackTorrence <jacktorrence420@gmx.com>
2017-10-16 12:23:01 +0100

    Add project files.

---
+++ src/Equinox.Infra.Data/Context/EquinoxContext.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using Equinox.Domain.Models;
+using Equinox.Infra.Data.Mappings;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Equinox.Infra.Data.Context
+{
+    public class EquinoxContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CustomerMap());
+                        
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // get the configuration from the app settings
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            
+            // define the database to use
+            optionsBuilder.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+        }
+    }
+}
